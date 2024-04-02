4ec0dac36a7dccb51ee21aa06704204c98cd92d6
Mathieu Brujan <mathieu.brujan@epitech.eu>
2024-04-02 19:23:18 +0900

    feat(day_12): complete part 1 and part 2

--- day_12/main.go
+++ day_12/main.go
@@ -3,41 +3,113 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func test(value any) {
+// receives a input string starting with "
+// terminating with "
+// parse the string and return it
+// returns the remainder of the string
+func parseString(input string) (string, string) {
+	result, follow, _ := strings.Cut(input[1:], "\"")
+	return result, follow
+}
+
+// receives a string with a continuation of number
+// followed by the rest of the thing to parse
+// parse the string, convert it to int and return it
+// along with the remainder of the string
+func parseInt(input string) (int, string) {
+	var i int
+	for i = 0; input[i] == '-' || input[i] >= '0' && input[i] <= '9'; i++ {
+	}
+	number, _ := strconv.ParseInt(input[:i], 10, 32)
+	return int(number), input[i:]
+}
+
+func parseArray(input string) ([]any, string) {
+	next := input
+	array := make([]any, 0)
+	for next[0] != ']' {
+		value, continuation := parseUnknown(next[1:])
+		array = append(array, value)
+		next = continuation
+	}
+	return array, next[1:]
+}
+
+// This function receive an input string that starts with {
+// and which is a correct object until the terminating }
+// This function will parse the entire object, and return it
+// With the remainder of the string
+func parseObject(input string) (map[string]any, string) {
+	next := input
+	object := make(map[string]any)
+	for next[0] != '}' {
+		key, continuation := parseString(next[1:])
+		value, continuation := parseUnknown(continuation[1:])
+		object[key] = value
+		next = continuation
+	}
+	return object, next[1:]
+}
+
+func parseUnknown(input string) (any, string) {
+	switch input[0] {
+	case '"':
+		return parseString(input)
+	case '[':
+		return parseArray(input)
+	case '{':
+		return parseObject(input)
+	default:
+		return parseInt(input)
+	}
+}
+
+func parseContent(content string) map[string]any {
+	object, _ := parseObject(content)
+	return object
+}
+
+func sumIntInArray(array []any, redMatters bool) int {
+	result := 0
+	for _, value := range array {
+		number, _ := getSumIntOfAnyAndIfRed(value, redMatters)
+		result += number
+	}
+	return result
+}
+
+func sumIntInObject(object map[string]any, redMatters bool) int {
+	result := 0
+	for _, value := range object {
+		number, isRedString := getSumIntOfAnyAndIfRed(value, redMatters)
+		if redMatters && isRedString {
+			return 0
+		}
+		result += number
+	}
+	return result
+}
+
+func getSumIntOfAnyAndIfRed(value any, redMatters bool) (int, bool) {
 	switch value.(type) {
 	case int:
-		fmt.Println("Int")
-	case string:
-		fmt.Println("String")
+		return value.(int), false
 	case map[string]any:
-		fmt.Println("Object")
+		return sumIntInObject(value.(map[string]any), redMatters), false
 	case []any:
-		fmt.Println("Array")
+		return sumIntInArray(value.([]any), redMatters), false
 	default:
-		fmt.Println("Unknown")
+		return 0, value.(string) == "red"
 	}
 }
 
-func create() map[string]any {
-	obj := make(map[string]any)
-	obj["string"] = "hello"
-	return obj
-}
-
-func solve(content string) {
-	obj := make(map[string]any)
-	obj["int"] = 32
-	array := make([]any, 0)
-	array = append(array, 3)
-	obj["array"] = array
-	obj["string"] = "test"
-	obj["obj"] = create()
-
-	for _, value := range obj {
-		test(value)
-	}
+func solve(object map[string]any) {
+	fmt.Println("Result of part 1:", sumIntInObject(object, false))
+	fmt.Println("Result of part 2:", sumIntInObject(object, true))
 }
 
 func main() {
@@ -46,5 +118,6 @@ func main() {
 		panic(err)
 	}
 	content := string(dat)
-	solve(content)
+	object := parseContent(content)
+	solve(object)
 }
